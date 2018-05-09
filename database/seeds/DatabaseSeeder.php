266b3d207a4fc025a734bc12e80d96e125fd8f71
Anderson Rodrigues Palmeira <andersonarp2810@gmail.com>
2018-05-09 14:48:34 -0300

    seed e boot

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,5 +12,9 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+        App\User::create([
+            "email" => "as@df.com",
+            "password" => bcrypt('123456')
+        ]);
     }
 }
