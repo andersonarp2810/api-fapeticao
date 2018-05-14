5483f49a05208242990e0f6d2e563a3d27445b35
Anderson Rodrigues Palmeira <andersonarp2810@gmail.com>
2018-05-14 16:49:23 -0300

    User com tipo & mais seeds

--- app/User.php
+++ app/User.php
@@ -17,7 +17,7 @@ class User extends Authenticatable implements JWTSubject
      * @var array
      */
     protected $fillable = [
-        'email', 'password',
+        'email', 'password', 'tipo',
     ];
 
     /**
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\User;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,9 +13,26 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
-        App\User::create([
-            "email" => "as@df.com",
-            "password" => bcrypt('123456')
+        User::create([
+            "email" => "pro@fes.sor",
+            "password" => bcrypt('123456'),
+            "tipo" => 1
         ]);
+        User::create([
+            "email" => "alu@no.com",
+            "password" => bcrypt('123456'),
+            "tipo" => 2
+        ]);
+        User::create([
+            "email" => "ad@min.com",
+            "password" => bcrypt('123456'),
+            "tipo" => 3
+        ]);
+        User::create([
+            "email" => "def@en.sor",
+            "password" => bcrypt('123456'),
+            "tipo" => 4
+        ]);
+        
     }
 }
