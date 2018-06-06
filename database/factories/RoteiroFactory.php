074a39da458e5874bbe7601daeb90b3eff1dfcc3
Anderson Rodrigues Palmeira <andersonarp2810@gmail.com>
2018-06-06 16:07:57 -0300

    factory errado?

--- database/factories/RoteiroFactory.php
+++ database/factories/RoteiroFactory.php
@@ -4,7 +4,7 @@ use Faker\Generator as Faker;
 
 $factory->define(App\Roteiro::class, function (Faker $faker) {
     return [
-        'titulo' => $faker->word(),
+        'titulo' => $faker->word,
         'texto' => $faker->text(),
         'id_professor' => function(){
             return App\Professor::all()->random();
