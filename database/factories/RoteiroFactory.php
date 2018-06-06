4353c892f1702ee751c07627886895bf4321a1bb
Anderson Rodrigues Palmeira <andersonarp2810@gmail.com>
2018-06-06 15:52:07 -0300

    oh besteira

--- database/factories/RoteiroFactory.php
+++ database/factories/RoteiroFactory.php
@@ -4,7 +4,7 @@ use Faker\Generator as Faker;
 
 $factory->define(App\Roteiro::class, function (Faker $faker) {
     return [
-        'titulo' => $factory->word(),
+        'titulo' => $faker->word(),
         'texto' => $faker->text(),
         'id_professor' => function(){
             return App\Professor::all()->random();
